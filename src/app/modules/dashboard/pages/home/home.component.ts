c6007815411153c1e609e3ce81f63a80a43614c7
Katherine Droguett <ktysakura_jb.30stm@hotmail.com>
2025-03-03 20:38:04 -0300

    home page

--- src/app/modules/dashboard/pages/home/home.component.ts
+++ src/app/modules/dashboard/pages/home/home.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentsService } from '../../../../core/services/students.service';
+import { CoursesService } from '../../../../core/services/courses.service';
+import { InscriptionsService } from '../../../../core/services/inscriptions.service';
 
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  nStudents: number = 0;
+  nCourses: number = 0;
+  nInscriptions: number = 0;
+
+  constructor(
+    private studentsService: StudentsService,
+    private coursesService: CoursesService,
+    private inscriptionsService: InscriptionsService
+  ){}
+
+  ngOnInit(): void {
+    this.studentsService.getStudentsObservable().subscribe({
+      next: (students) => {
+        this.nStudents = students.length;
+      }
+    })
+
+    this.coursesService.getCourses().subscribe({
+      next: (courses) => {
+        this.nCourses = courses.length;
+      }
+    })
+
+    this.inscriptionsService.getInscriptions().subscribe({
+      next: (inscriptions) => {
+        this.nInscriptions = inscriptions.length;
+      }
+    })
+  }
 
 }
